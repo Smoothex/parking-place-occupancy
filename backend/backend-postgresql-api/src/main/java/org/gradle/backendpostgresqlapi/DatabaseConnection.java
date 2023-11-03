c4a62151fe47c2fc831200601a367b7f7d10b24f
gogokotsev00 <Kotsev75@gmail.com>
2023-11-03 12:15:20 +0100

    Added configuration for local testing

--- backend/backend-postgresql-api/src/main/java/org/gradle/backendpostgresqlapi/DatabaseConnection.java
+++ backend/backend-postgresql-api/src/main/java/org/gradle/backendpostgresqlapi/DatabaseConnection.java
@@ -6,26 +6,32 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 @SpringBootApplication
 public class DatabaseConnection {
 
 	public static void main(String[] args) {
-        String dbUser = "myuser";
-        String dbPassword = "mypassword";
-        String url = "jdbc:postgresql://postgres:5432/mydb";
+        boolean isExecutedInDocker = true; // change when using it locally for testing
+        String dbUser = "parkuser";
+        String dbName = "parking_spots_db";
+        String hostName = isExecutedInDocker ? "postgres" : "localhost";
+        String containerPort = isExecutedInDocker ? "5432" : "32768";
+        String url = "jdbc:postgresql://" + hostName + ":" + containerPort + "/" + dbName;
+        Properties connectionProp = new Properties();
+        connectionProp.setProperty("user", dbUser);
         try {
-            Thread.sleep(10000); // sleep for 10s so that PostgreSQL can initialize properly
-            Connection connection = DriverManager.getConnection(url, dbUser, dbPassword);
+            if (isExecutedInDocker) {
+                Thread.sleep(10000); // sleep for 10s so that PostgreSQL can initialize properly
+            }
+            Connection connection = DriverManager.getConnection(url, connectionProp);
             if (connection != null) {
                 System.out.println("Connected to the database!");
                 // Perform database operations here
 
                 connection.close();
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
+        } catch (SQLException | InterruptedException e) {
             e.printStackTrace();
         }
     }
