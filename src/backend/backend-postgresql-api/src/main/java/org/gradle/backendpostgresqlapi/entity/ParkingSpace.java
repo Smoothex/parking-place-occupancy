e069851be4a3f9b15acd2db16abc1eb3f671a722
gogokotsev00 <Kotsev75@gmail.com>
2023-11-10 13:17:58 +0100

    Created an entity object for parking space and updated the DTO one

--- src/backend/backend-postgresql-api/src/main/java/org/gradle/backendpostgresqlapi/entity/ParkingSpace.java
+++ src/backend/backend-postgresql-api/src/main/java/org/gradle/backendpostgresqlapi/entity/ParkingSpace.java
@@ -1,22 +1,32 @@
-package org.gradle.backendpostgresqlapi.dto;
+package org.gradle.backendpostgresqlapi.entity;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import static org.gradle.backendpostgresqlapi.util.JsonHandler.getCoordinatesFromGeoJson;
+import static org.gradle.backendpostgresqlapi.util.JsonHandler.*;
 
 @Getter
 @Setter
-public class ParkingSpaceDTO {
+@Entity
+@Table(name = "parking_spaces", schema = "public")
+public class ParkingSpace {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ps_id")
     private int id;
+
+    @Column(name = "ps_coordinates", nullable = false)
     private String geoJson;
+
+    @Column(name = "ps_occupied")
     private boolean occupied;
 
-    public ParkingSpaceDTO(int id, String geoJson) {
-        this.id = id;
-        this.geoJson = geoJson;
+    // default constructor only for the sake of JPA (See https://spring.io/projects/spring-data-jpa)
+    protected ParkingSpace() {
+
     }
 
     @Override
