7c822e027babf16b0fba5ba6004966248e2a8634
Smoothex <momchildp@gmail.com>
2023-11-12 18:14:03 +0100

    add hibernate-spatial, api endpoints, json methods

--- src/backend/backend-postgresql-api/src/main/java/org/gradle/backendpostgresqlapi/repository/GeospatialRepo.java
+++ src/backend/backend-postgresql-api/src/main/java/org/gradle/backendpostgresqlapi/repository/GeospatialRepo.java
@@ -10,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
+import org.locationtech.jts.geom.Polygon;
 
 @Repository
 @Transactional
@@ -17,9 +18,6 @@ public interface GeospatialRepo extends JpaRepository<ParkingSpace, Integer> {
 
     String CREATE_TABLE_SQL = "CREATE TABLE IF NOT EXISTS parking_spaces (ps_id SERIAL PRIMARY KEY, ps_coordinates GEOGRAPHY(POLYGON, 4326), ps_occupied BOOLEAN DEFAULT 'f')";
     String CREATE_INDEX_SQL = "CREATE INDEX IF NOT EXISTS ps_coordinates_idx ON parking_spaces USING GIST (ps_coordinates)";
-    String INSERT_GEOJSON_SQL = "INSERT INTO parking_spaces (ps_coordinates) VALUES (ST_GeomFromGeoJSON(:parkSpace))";
-    String SELECT_PARKING_SPACES_SQL = "SELECT ps_id, ST_AsGeoJSON(ps_coordinates) AS ps_coordinates, ps_occupied FROM parking_spaces";
-    String SELECT_PARKING_SPACE_BY_ID_SQL = "SELECT ps_id, ST_AsGeoJSON(ps_coordinates) AS ps_coordinates, ps_occupied FROM parking_spaces WHERE ps_id = :id";
 
     @Modifying
     @Query(value = CREATE_TABLE_SQL, nativeQuery = true)
@@ -30,14 +28,12 @@ public interface GeospatialRepo extends JpaRepository<ParkingSpace, Integer> {
     void createIndex();
 
     @Modifying
-    @Query(value = INSERT_GEOJSON_SQL, nativeQuery = true)
-    void insertParkingSpace(@Param("parkSpace") String parkingSpace);
-
-    @Query(value = SELECT_PARKING_SPACES_SQL, nativeQuery = true)
-    List<ParkingSpace> getAllParkingSpaces();
-
-    @Query(value = SELECT_PARKING_SPACE_BY_ID_SQL, nativeQuery = true)
-    Optional<ParkingSpace> findById(@Param("id") Integer id);
+    default void insertParkingSpace(Polygon polygon) {
+        ParkingSpace parkingSpace = new ParkingSpace();
+        parkingSpace.setPolygon(polygon);
+        parkingSpace.setOccupied(false);
+        this.save(parkingSpace);
+    }
 
     List<ParkingSpace> findByOccupied(boolean occupied);
 
