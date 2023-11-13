13297c126a1db0c7d0a56c2b5f6d6e95f2bfb3cc
krutarth4 <krutarthparwal@gmail.com>
2023-11-13 16:12:47 +0100

    OSM integrated

---
+++ src/frontend/src/app/components/map/map.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent {
+
+  private map:any;
+
+  private initMap(): void {
+    this.map = L.map('map').setView([51.505, -0.09], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(this.map);
+
+  }
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+
+}
