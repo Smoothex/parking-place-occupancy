eb51281b9c86364f82f1328ce929b28e9be51f09
Smoothex <momchildp@gmail.com>
2023-11-14 10:20:26 +0100

    add polygon area calculation in squared meters

--- src/backend/backend-postgresql-api/src/main/java/org/gradle/backendpostgresqlapi/service/GeospatialService.java
+++ src/backend/backend-postgresql-api/src/main/java/org/gradle/backendpostgresqlapi/service/GeospatialService.java
@@ -1,31 +1,36 @@
 package org.gradle.backendpostgresqlapi.service;
 
 import lombok.extern.slf4j.Slf4j;
+
 import org.gradle.backendpostgresqlapi.entity.ParkingSpace;
 import org.gradle.backendpostgresqlapi.repository.GeospatialRepo;
 import org.gradle.backendpostgresqlapi.util.JsonHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-
 import static org.gradle.backendpostgresqlapi.util.JsonHandler.getJsonDataFromFile;
 import static org.gradle.backendpostgresqlapi.util.JsonHandler.getPolygonsFromGeoJson;
+
 import org.locationtech.jts.geom.Polygon;
+import org.locationtech.jts.geom.util.GeometryTransformer;
+import org.locationtech.jts.geom.CoordinateSequence;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.proj4j.CRSFactory;
+import org.locationtech.proj4j.CoordinateTransform;
+import org.locationtech.proj4j.CoordinateTransformFactory;
+import org.locationtech.proj4j.ProjCoordinate;
+import org.locationtech.proj4j.CoordinateReferenceSystem;
 
 @Slf4j
 @Service
 public class GeospatialService {
-
     private static final String GEOJSON_FILE = "classpath:data.geojson";
-
+     
     @Autowired
     private GeospatialRepo geospatialRepo;
 
@@ -94,7 +99,55 @@ public class GeospatialService {
     public Optional<Double> calculateAreaOfParkingSpace(int id) {
         return geospatialRepo.findById(id)
                              .map(ParkingSpace::getPolygon)
-                             .map(Polygon::getArea);
+                             .map(this::transformAndCalculateArea);
     }
-    
+
+    /**
+     * Transforms the Coordinate Reference System (CRS) of a polygon and calculates its area.
+     * Handles the transformation by iterating over each coordinate of the polygon, applying 
+     * the coordinate conversion, and then computing the area of the resultant geometry.
+     * 
+     * The source CRS is in WGS84 (World Geodetic System 1984) format, which is often used when working
+     * with GIS data and is based on a reference ellipsoid (i.e., the earth). Coordinates are represented as latitude and longitude.
+     * 
+     * The target CRS is in UTM (Universal Transverse Mercator) format, which is suited for spatial analysis
+     * and uses Cartesian coordinate system with linear units (meters) for its coordinates.
+     * 
+     * @param polygon The Polygon object defined in WGS84 coordinates to be transformed and calculated.
+     * @return The area of the polygon in square meters after transformation to UTM coordinates.
+     */
+    private double transformAndCalculateArea(Polygon polygon) {
+        // Define the source and target CRS
+        CRSFactory crsFactory = new CRSFactory();
+        CoordinateReferenceSystem sourceCRS = crsFactory.createFromName("EPSG:4326"); // WGS84
+        CoordinateReferenceSystem targetCRS = crsFactory.createFromName("EPSG:25833"); // UTM
+
+        // Create a factory for coordinate transformations
+        CoordinateTransformFactory ctFactory = new CoordinateTransformFactory();
+        CoordinateTransform transform = ctFactory.createTransform(sourceCRS, targetCRS);
+
+        // Create a custom GeometryTransformer
+        GeometryTransformer transformer = new GeometryTransformer() {
+            @Override
+            protected CoordinateSequence transformCoordinates(CoordinateSequence coordinates, Geometry parent) {
+                CoordinateSequence transformedCoordinates = coordinates.copy();
+                for (int i = 0; i < coordinates.size(); i++) {
+                    // Create a ProjCoordinate from the current coordinate
+                    ProjCoordinate srcCoord = new ProjCoordinate(coordinates.getX(i), coordinates.getY(i));
+                    // Transform the source coordinate to the target CRS
+                    ProjCoordinate dstCoord = transform.transform(srcCoord, new ProjCoordinate());
+                    // Update the transformed coordinates sequence with the new values
+                    transformedCoordinates.setOrdinate(i, 0, dstCoord.x);
+                    transformedCoordinates.setOrdinate(i, 1, dstCoord.y);
+                }
+                return transformedCoordinates;
+            }
+        };
+
+        // Transform the geometry
+        Geometry transformedGeom = transformer.transform(polygon);
+
+        // Return the area of the transformed geometry
+        return transformedGeom.getArea();
+    }
 }
