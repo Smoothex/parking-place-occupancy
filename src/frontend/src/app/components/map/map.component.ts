aa38874edcd970b1274af4c7d271d06b8b4e6e60
krutarth4 <krutarthparwal@gmail.com>
2023-11-15 19:02:25 +0100

    map markers added

--- src/frontend/src/app/components/map/map.component.ts
+++ src/frontend/src/app/components/map/map.component.ts
@@ -5,65 +5,77 @@ import { RestAPIService } from 'src/app/services/restAPI/rest-api.service';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.css'],
 })
 export class MapComponent {
+  private map: any;
+  private markerLayer: any;
 
-  private map:any;
+  icon = {
+    icon: L.icon({
+      iconSize: [25, 41],
+      iconAnchor: [12.5, 41],
+      // specify the path here
+      iconUrl: '../../assets/marker-icon.png',
+      shadowUrl: '../../assets/marker-shadow.png',
+    }),
+  };
 
   private initMap(): void {
     this.map = L.map('map').setView([52.54412, 13.352], 15);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 24,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      maxZoom: 24,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     }).addTo(this.map);
-    
-    this.map.on('click',this.giveCoordinate)
 
+    this.map.on('click', this.giveCoordinate.bind(this));
   }
 
   giveCoordinate(e: any) {
-    console.log("clicked on map", e.latlng)
-    var marker = L.marker(e.latlng)
-      marker.addTo(this.map)
-    
+    console.log('clicked on map', e.latlng);
+    // var marker = L.marker(e.latlng, this.icon)
+    //   marker.addTo(this.map)
+    // todo: any markers present remove it automatically
   }
 
-  constructor(private restApi: RestAPIService) { }
+  constructor(private restApi: RestAPIService) {}
 
   ngAfterViewInit(): void {
     this.initMap();
     setTimeout(() => {
-      
-      this.markParkingPlaces()
+      this.markParkingPlaces();
     }, 500);
   }
-  
 
-  markParkingPlaces(): void{
-    this.restApi.getAllParkingSpaces().then(data => {
+  markParkingPlaces(): void {
+    this.restApi.getAllParkingSpaces().then((data) => {
       data.forEach((element: any) => {
-
-        const parseElement = JSON.parse(element)
-        console.log(parseElement)
-        var coordinatesArray = parseElement.polygon.coordinates
-        const arrayOfArrays = coordinatesArray.map((obj:any) => [obj.y, obj.x]);
-        console.log(arrayOfArrays)
-        var polygon = L.polygon(arrayOfArrays).addTo(this.map)
-          .bindPopup("Id " + parseElement.id + "<br>" + "occupied : " + JSON.stringify(parseElement.occupied));
-       /*  polygon.on('click',  (event) =>{
-          console.log(event + "parse" + parseElement.id)
-          arrayOfArrays.forEach((elem:any) => {
-            L.marker(elem).addTo(this.map)
-          })
-        }) */
+        const parseElement = JSON.parse(element);
+        console.log(parseElement);
+        var coordinatesArray = parseElement.polygon.coordinates;
+        const arrayOfArrays = coordinatesArray.map((obj: any) => [
+          obj.y,
+          obj.x,
+        ]);
+        console.log(arrayOfArrays);
+        var polygon = L.polygon(arrayOfArrays)
+          .addTo(this.map)
+          .bindPopup(
+            'Id ' +
+              parseElement.id +
+              '<br>' +
+              'occupied : ' +
+              JSON.stringify(parseElement.occupied) +
+              '<br> <button>Edit</button></button>'
+          );
+        polygon.on('click', (event) => {
+          console.log(event + 'parse' + parseElement.id);
+          arrayOfArrays.forEach((elem: any) => {
+            L.marker(elem, this.icon).addTo(this.map);
+          });
+        });
       });
-
-      
-
-    })
-    
+    });
   }
-
-
 }
